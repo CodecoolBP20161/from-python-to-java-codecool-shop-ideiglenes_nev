d78191eef517f1c074edf1482ba7ea172ffc3901
Swaggerino <bjoci93@gmail.com>
2016-11-15 12:01:15 +0100

    Added a keyboard + all products appear at first page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,6 +25,8 @@ public class Main {
         // Always add generic routes to the end
         get("/", ProductController::renderProducts, new ThymeleafTemplateEngine());
 
+
+
         // Add this line to your project to enable the debug screen
         enableDebugScreen();
     }
@@ -44,11 +46,14 @@ public class Main {
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory keyboards = new ProductCategory("Keyboard", "Hardware", "A keyboard");
+        productCategoryDataStore.add(keyboards);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Wonderful keyboard", 20, "USD", "Used for typing.", keyboards, amazon));
 
     }
 
