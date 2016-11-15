4dc60655e905838edb9195fdcb8c37b8e069e598
arvisura87 <jakab.tamas@me.com>
2016-11-15 12:20:23 +0100

    new product cat and prod added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
+import com.codecool.shop.controller.ProductController;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 import static spark.Spark.*;
 import static spark.debug.DebugScreen.enableDebugScreen;
-
-import com.codecool.shop.controller.ProductController;
-import com.codecool.shop.dao.*;
-import com.codecool.shop.dao.implementation.*;
-import com.codecool.shop.model.*;
-import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
 
@@ -43,17 +49,24 @@ public class Main {
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
 
+        Supplier apple = new Supplier("Apple", "Mobile");
+        supplierDataStore.add(apple);
+
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
         ProductCategory keyboards = new ProductCategory("Keyboard", "Hardware", "A keyboard");
         productCategoryDataStore.add(keyboards);
+        ProductCategory mobile = new ProductCategory("Mobile", "Hardware", "A mobile, you can call anybody");
+        productCategoryDataStore.add(mobile);
+
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
         productDataStore.add(new Product("Wonderful keyboard", 20, "USD", "Used for typing.", keyboards, amazon));
+        productDataStore.add(new Product("Apple iPhone 7", 799, "USD", "The revolution of mobilephone.", mobile, apple));
 
     }
 
