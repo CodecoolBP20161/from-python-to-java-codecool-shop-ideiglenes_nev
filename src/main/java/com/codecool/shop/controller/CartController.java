d36197d951146d90d3d0b0b65ba2c7f2defe8e19
arvisura87 <jakab.tamas@me.com>
2016-11-17 13:37:43 +0100

    req id working

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,6 +1,8 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.model.Cart;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -11,9 +13,18 @@ import spark.Response;
 public class CartController {
     public static ModelAndView addToCart(Request req, Response res) {
 
-        Cart cart = new Cart();
-        cart.add(req.id);
-        System.out.println("mukodik");
+        int productId = Integer.parseInt(req.params(":product_id"));
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        Product product = productDataStore.find(productId);
+
+//        if (req.session().isNew()){
+//            Cart cart = new Cart();
+//
+//        } else {
+//
+//        }
+
+        System.out.println(productId);
         res.redirect("/");
         return null;
     }
