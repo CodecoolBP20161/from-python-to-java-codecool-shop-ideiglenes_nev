137f8cc2d175def0fc67696d1815c08b40a46ff6
arvisura87 <jakab.tamas@me.com>
2016-11-29 13:57:29 +0100

    added create_table sql

---
+++ src/main/resources/sql/create_table.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS product_category;
+DROP TABLE IF EXISTS supplier;
+
+
+CREATE TABLE product_category
+(
+  id SERIAL PRIMARY KEY,
+  name varchar(255),
+  department varchar(255),
+  description TEXT
+);
+
+CREATE TABLE supplier
+(
+  id SERIAL PRIMARY KEY,
+  name varchar(255),
+  description TEXT
+);
+
+CREATE TABLE product
+(
+  id SERIAL PRIMARY KEY,
+  defaultPrice REAL,
+  defaultCurrency VARCHAR(255),
+  productCategory INTEGER REFERENCES product_category(id),
+  supplier INTEGER REFERENCES supplier(id)
+);
