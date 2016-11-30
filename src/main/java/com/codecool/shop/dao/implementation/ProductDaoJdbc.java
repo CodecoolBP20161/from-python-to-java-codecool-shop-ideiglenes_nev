a8aee87343eb113acfbd6e951f168a6088b9579e
arvisura87 <jakab.tamas@me.com>
2016-11-30 10:29:41 +0100

    jdbc files added

---
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -0,0 +1,10 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductDao;
+
+/**
+ * Created by jakab on 2016.11.29..
+ */
+public class ProductDaoJdbc implements ProductDao{
+
+}
