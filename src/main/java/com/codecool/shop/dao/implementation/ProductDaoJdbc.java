2013448bfa6c6086283ba41c03ad3cd200e6008b
arvisura87 <jakab.tamas@me.com>
2016-11-30 13:29:41 +0100

    example product jdbc

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -1,10 +1,82 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.sql.*;
+import java.util.List;
 
 /**
  * Created by jakab on 2016.11.29..
  */
 public class ProductDaoJdbc implements ProductDao{
 
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/codecoolshop";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "57931111";
+
+    @Override
+    public void add(Product product) {
+
+        String query = "INSERT INTO product (name, defaultPrice, defaultCurrency, productCategory, supplier) VALUES (?, ?, ?, ?, ?);";
+        try (Connection connection = getConnection()){;
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, product.getName());
+            stmt.setFloat(2, product.getDefaultPrice());
+            stmt.setString(3, product.getDefaultCurrency().toString());
+            stmt.setInt(4, product.getProductCategory().getId());
+            stmt.setInt(5, product.getSupplier().getId());
+
+
+            stmt.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+        ){
+            statement.execute(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
