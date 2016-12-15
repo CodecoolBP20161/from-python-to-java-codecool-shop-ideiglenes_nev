b74d0e4794b7e7d540a6eb7c39d1f92945e5dc06
arvisura87 <jakab.tamas@me.com>
2016-12-15 14:15:50 +0100

    footer bug fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -81,7 +81,7 @@ public class Main {
         productDataStore.add(new Product("Samsung Galaxy Note 7", 390, "USD", "Explodingly awesome.", mobile, samsung));
         productDataStore.add(new Product("Amazon keyboard", 20, "USD", "Used for typing.", keyboard, amazon));
         productDataStore.add(new Product("Razer Deathstalker", 80, "USD", "Fully programmable slim chiclet keycaps.", keyboard, razer));
-        productDataStore.add(new Product("Apple Keyboard", 50, "USD", "The Apple Keyboard with Numeric Keypad features an elegant, ultrathin anodized aluminum enclosure with low-profile keys that provide a crisp, responsive feel.", keyboard, apple));
+        productDataStore.add(new Product("Apple Keyboard", 50, "USD", "The Apple Keyboard with Numeric Keypad.", keyboard, apple));
         productDataStore.add(new Product("Razer Kraken 7.1", 80, "USD", "The perfect balance of weight, functionality, and performance, the Razer Kraken is undisputedly the most popular E-sports gaming headset out there.", headphone, razer));
     }
 
