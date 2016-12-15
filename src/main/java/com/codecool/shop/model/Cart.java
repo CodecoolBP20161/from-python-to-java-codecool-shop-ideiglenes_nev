e420055ab62c41d2e4c51bbfe2487347d1563b77
arvisura87 <jakab.tamas@me.com>
2016-12-15 10:12:36 +0100

    list products in cart to test rewiew page and for the styling

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -7,10 +7,12 @@ import java.util.ArrayList;
  */
 public class Cart extends AbstractCart{
     public void add(Product product){
+
         lineItems.add(product);
     }
 
-    public ArrayList<Product> getLineItems(){
+    public static ArrayList<Product> getLineItems(){
+
         return lineItems;
     }
 }
