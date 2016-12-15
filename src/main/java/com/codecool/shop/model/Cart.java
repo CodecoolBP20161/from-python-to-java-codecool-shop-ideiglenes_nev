f0b859e194df9477736b9bd4cda74d44538244c6
arvisura87 <jakab.tamas@me.com>
2016-12-15 11:33:58 +0100

    added backend code to show details in shoppingcart on review page

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -1,18 +1,62 @@
 package com.codecool.shop.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jakab on 2016.11.17..
  */
 public class Cart extends AbstractCart{
-    public void add(Product product){
+    public static int lineAll=0;
+    public static ArrayList<LineItem> lineItems = new ArrayList<>();
+    private int totalQuantity;
+    public static float totalPrice=0;
 
-        lineItems.add(product);
+    public static int getLineAll(){
+        return lineAll;
     }
 
-    public static ArrayList<Product> getLineItems(){
+    public void add(Product product) {
+        boolean in = false;
+        for (LineItem item : lineItems) {
+            if (item.getProduct().getId() == product.getId()) {
+                item.increaseQuantity(1);
+                lineAll+=1;
+                totalPrice += item.getProduct().getDefaultPrice();
 
+                in = true;
+            }
+        }
+
+        if (in == false) {
+            lineItems.add(new LineItem(product, 1));
+            lineAll+=1;
+            totalPrice += product.getDefaultPrice();
+        }
+    }
+
+    public int getTotalQuantity() {
+        this.totalQuantity = 0;
+        for (LineItem item : lineItems) {
+            this.totalQuantity += item.getQuantity();
+        }
+        return this.totalQuantity;
+    }
+
+    public static List<LineItem> lineItems() {
         return lineItems;
     }
+
+    public void removeProduct(Product product){
+        for (LineItem item : lineItems) {
+            if (item.getProduct().getId() == product.getId()) {
+                item.decreaseQuantity(1);
+            }
+        }
+    }
+
+    public static float getTotalPrice() {
+        return totalPrice;
+    }
+
 }
