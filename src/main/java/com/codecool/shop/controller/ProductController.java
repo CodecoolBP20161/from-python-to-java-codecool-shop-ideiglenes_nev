e8fa0b9629b176c7df1fe8bd8c57703cd10bdc5a
arvisura87 <jakab.tamas@me.com>
2017-01-25 11:44:24 +0100

    Avatar service added to the shop

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -33,13 +33,14 @@ public class ProductController {
         params.put("supplier", "All suppliers");
         params.put("cart", Cart.getLineAll());
         params.put("top5", Top5Controller.getTop5(req, res));
+        params.put("avatar", "http://" + AvatarController.getAvatar(req, res));
 
         req.session().attribute("lastURL", req.pathInfo());
 
         return new ModelAndView(params, "product/index");
     }
 
-    public static ModelAndView renderCategory(Request req, Response res) {
+    public static ModelAndView renderCategory(Request req, Response res) throws IOException, URISyntaxException {
         String categoryId = req.params(":category_id");
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
@@ -52,12 +53,13 @@ public class ProductController {
         params.put("suppliers", supplierDataStone.getAll());
         params.put("supplier", "All suppliers");
         params.put("cart", Cart.getLineAll());
+        params.put("avatar", "http://" + AvatarController.getAvatar(req, res));
 
         req.session().attribute("lastURL", req.pathInfo());
 
         return new ModelAndView(params, "product/category");
     }
-    public static ModelAndView renderSupplier(Request req, Response res) {
+    public static ModelAndView renderSupplier(Request req, Response res) throws IOException, URISyntaxException {
         String supplierId = req.params(":supplier_id");
         ProductDao productDataStore = ProductDaoMem.getInstance();
         SupplierDao supplierDataStone = SupplierDaoMem.getInstance();
@@ -70,18 +72,19 @@ public class ProductController {
         params.put("category", "All categories");
         params.put("categories", productCategoryDataStore.getAll());
         params.put("cart", Cart.getLineAll());
+        params.put("avatar", "http://" + AvatarController.getAvatar(req, res));
 
         req.session().attribute("lastURL", req.pathInfo());
 
         return new ModelAndView(params, "product/supplier");
     }
 
-    public static ModelAndView renderReview(Request req, Response res) {
+    public static ModelAndView renderReview(Request req, Response res) throws IOException, URISyntaxException {
         Map params = new HashMap<>();
         params.put("lineItems", Cart.lineItems());
         params.put("cart", Cart.getLineAll());
         params.put("total", Cart.getTotalPrice());
-
+        params.put("avatar", "http://" + AvatarController.getAvatar(req, res));
 
         req.session().attribute("lastURL", req.pathInfo());
 
